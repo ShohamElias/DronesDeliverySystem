6d5ddb50bd671e3eface1e03ba6d76c962840413
adiRozental <arozenta@g.jct.ac.il>
2021-10-10 14:05:53 +0300

    intial + welcome

---
+++ Targil0/Program6442.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome6442()
+        {
+            Console.WriteLine("I'm also here!");
+        }
+    }
+}
