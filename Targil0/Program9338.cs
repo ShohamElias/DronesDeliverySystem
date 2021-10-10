83246a3efcda391c75aab347a199e7e570ba0a32
ShohamElias <shelias@g.jct.ac.il>
2021-10-10 13:39:30 +0300

    Inital code + welcome function

---
+++ Targil0/Program9338.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9338();
+            Welcome6442();
+            Console.ReadKey();
+        }
+        static partial void Welcome6442();
+        private static void Welcome9338()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
