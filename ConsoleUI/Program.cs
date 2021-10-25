eb4e632e86e832ce1c22706181b80a8a3614384d
adiRozental <arozenta@g.jct.ac.il>
2021-10-25 15:19:40 +0300

    changes3

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,9 +7,54 @@ namespace ConsoleUI
 {
     class Program
     {
+        enum MenuOpt { Exit, Add, Update, ShowOne, ShowList }
+        enum AddOpt { Exit, AddDrone, AddStation, AddParcel, AddCustomer }
+        enum UpdateOpt { Exit, AddDrone, AddStation, AddParcel, AddCustomer }
+        enum ShowONEOpt { Exit, AddDrone, AddStation, AddParcel, AddCustomer }
+        enum ShowListOpt { Exit, AddDrone, AddStation, AddParcel, AddCustomer }
+        void PrintMenu()
+        {
+            MenuOpt mo;
+            int option;
+            bool flag = int.TryParse(Console.ReadLine(), out option);
+            mo = (MenuOpt)option;
+            switch (mo)
+            {
+                case MenuOpt.Exit:
+                    break;
+
+                case MenuOpt.Add: AddOpt addO;
+                     flag = int.TryParse(Console.ReadLine(), out option);
+                    addO = (AddOpt)option;
+                    switch (addO)
+                    {
+                        case AddOpt.Exit:
+                            break;
+                        case AddOpt.AddDrone:
+                            break;
+                        case AddOpt.AddStation:
+                            break;
+                        case AddOpt.AddParcel:
+                            break;
+                        case AddOpt.AddCustomer:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case MenuOpt.Update: 
+                    break;
+                case MenuOpt.ShowOne:
+                    break;
+                case MenuOpt.ShowList:
+                    break;
+                default:
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-             
         }
     }
 }
--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -14,14 +14,65 @@ namespace DalObject
             DataSource.Intialize();
         }
 
-        public void addDrone(int id, string model, WeightCategories maxWeight, DroneStatuses status , double battery)//static?
+        public void addDrone(Drone d)//static?
         {
-            Drone d = new Drone();
-            d.Id = id;
-            d.MaxWeight = maxWeight;
-            d.Model = model;
-            d.Status = status;
-            d.Battery = battery;
+            DataSource.drones[DataSource.Config.dronesI++] = d;
+        }
+
+        public void UpdateDrone(Drone d)//static?+ לעדכן ככה או ע"י ID
+        {
+            for (int i = 0; i < DataSource.Config.dronesI; i++)
+            {
+                if (DataSource.drones[i].Id == d.Id)
+                    DataSource.drones[i] = d;
+            }
+        }
+
+        public Drone GetDrone(int id)//static?+ 
+        {
+            for (int i = 0; i < DataSource.Config.dronesI; i++)
+            {
+                if (DataSource.drones[i].Id == id)
+                    return DataSource.drones[i];
+            }
+            return null;
+        }
+        public void addStation(Station s)//static?
+        {
+            //Station s = new Station();
+            //s.Id = id;
+            //s.Name = name;
+            //s.Lattitude = lattitude;
+            //s.Longitude = longitude;
+            //s.ChargeSlots = chargeSlots;
+            DataSource.stations[DataSource.Config.stationsI++] = s;
+        }
+
+        public void addCustomer(Customer cus)//static?
+        {
+            //Customer cus = new Customer();
+            //cus.Id = id;
+            //cus.Name = name;
+            //cus.Lattitude = lattitude;
+            //cus.Longitude = longitude;
+            //cus.Phone = phone;
+            DataSource.customers[DataSource.Config.customersI++] = cus;
+        }
+
+        public void addParcel(Parcel per)//static?
+        {
+            //Parcel per = new Parcel();
+            //per.Id = id;
+            //per.TargetId = targetId;
+            //per.Weight = weight;
+            //per.Priority = priority;
+            //per.Requested = requested;
+            //per.DroneId = droneId;
+            //per.SenderId = senderId;
+            //per.Scheduled = scheduled;
+            //per.PickedUp = pickedUp;
+            //per.Delivered = delivered;
+            DataSource.parcels[DataSource.Config.parcelsI++] = per;
         }
     }
 }
