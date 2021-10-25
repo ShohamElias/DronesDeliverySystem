e7fcc265676a9b064c3e8cd9ad1dfd439f6c8edd
adiRozental <arozenta@g.jct.ac.il>
2021-10-25 14:04:30 +0300

    CHANGES

---
+++ DAL/DalObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.DO;
+
+namespace DalObject
+{
+    public class DalObject
+    {
+        public DalObject()
+        {
+            DataSource.Intialize();
+        }
+
+        public void addDrone(int id, string model, WeightCategories maxWeight, DroneStatuses status , double battery)//static?
+        {
+            Drone d = new Drone();
+            d.Id = id;
+            d.MaxWeight = maxWeight;
+            d.Model = model;
+            d.Status = status;
+            d.Battery = battery;
+        }
+    }
+}
