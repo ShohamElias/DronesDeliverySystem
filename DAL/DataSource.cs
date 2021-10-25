07d27c6f1de3750d2e7fd6753b9cf276ee1c1d21
adiRozental <arozenta@g.jct.ac.il>
2021-10-25 13:34:22 +0300

    changes

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -7,7 +7,7 @@ using IDAL.DO;
 
 namespace DalObject
 {
-    class DataSource
+    public class DataSource
     {
 
         internal class Config
@@ -16,36 +16,42 @@ namespace DalObject
             internal static int stationsI = 0;
             internal static int customersI = 0;
             internal static int parcelsI = 0;
+            public static int countIdParcel = 1;
         }
 
         internal static Drone[] drones = new Drone[10];
         internal static Station [] stations = new Station[5];
         internal static Customer[] customers = new Customer[100];
         internal static Parcel[] parcels = new Parcel[1000];
-        static Random rand = new Random(DateTime.Now.Millisecond);
+        internal static Random rand = new Random(DateTime.Now.Millisecond);//not sure its internal
 
-        public static void  Intialize()
+        //internal static List<Drone> dronelist = new List<Drone>();
+        //internal static List<Station> stationsList = new List<Station>();
+        //internal static List<Customer> customersList = new List<Customer>();
+        //internal static List<Parcel> parcelsList = new List<Parcel>();
+
+        private static void createDrone(int num)
         {
-             //שדה סטטי של המחלקה
-            stations[0] = new Station();
-            stations[1] = new Station();
-            Config.stationsI += 2;
-            for (int i=0; i<5; i++)
+            for (int i=0; i < num; i++)
             {
-                drones[i] = new Drone()
+                drones[Config.dronesI++] = new Drone()
                 {
-                    Id = Config.dronesI++,
+                    Id = rand.Next(1000, 10001),
                     Model = "",
                     MaxWeight = (WeightCategories)rand.Next(3),
                     Status = (DroneStatuses)rand.Next(3),
                     Battery = rand.Next(101)
                 };
             }
-            for (int i = 0; i < 10; i++)
+        }
+
+        private static void createParcel(int num)
+        {
+            for (int i = 0; i < num; i++)
             {
-                parcels[i] = new Parcel()
+                parcels[Config.parcelsI++] = new Parcel()
                 {
-                    Id = Config.parcelsI++,
+                    Id = rand.Next(1000, 10001),
                     SenderId = 0,
                     TargetId = 0,
                     Weight = (WeightCategories)rand.Next(3),
@@ -57,19 +63,45 @@ namespace DalObject
                     Delivered = DateTime.Now
                 };
             }
-            for (int i = 0; i < 5; i++)
+        }
+
+        private static void createStation(int num)
+        {
+            for (int i = 0; i < num; i++)
             {
-                customers[i] = new Customer()
+                stations[Config.stationsI++] = new Station()
                 {
-                    Id = Config.customersI++,
+                    Id = rand.Next(1000, 10001),
+                    Name = " ",
+                    Longitude = 0,
+                    Lattitude = 0,
+                    ChargeSlots = rand.Next(11)
+                };
+            }
+        }
+
+        private static void creacustomer(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                customers[Config.customersI++] = new Customer()
+                {
+                    Id = rand.Next(100000000, 1000000000),
                     Name = "",
                     Phone = "",
                     Longitude = 0,
                     Lattitude = 0
                 };
-               
             }
         }
-
+        public static void  Intialize()
+        {
+             //שדה סטטי של המחלקה
+            
+            createDrone(5);
+            createStation(2);
+            createParcel(10);
+            creacustomer(10);
+        }
     }
 }
